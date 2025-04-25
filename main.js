81a07ebc41f623c8e249bbc3d67b31fcb47184d4
Nikola Bojinović <bojinovicnikola12@gmail.com>
2025-04-26 00:31:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWin = () => {
+    let win = new BrowserWindow({
+        width: 1500,
+        height: 800,
+        maximizable: true,
+        resizable: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            devTools: true,
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.maximize();
+
+    win.loadFile("./src/index.html");
+    //win.webContents.openDevTools();
+}
+
+app.on("ready", () => {
+    createWin();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWin();
+    })
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform != "darwin") app.quit();
+})
